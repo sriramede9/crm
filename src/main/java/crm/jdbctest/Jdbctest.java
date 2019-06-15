e27d537fce923c8c968d01bc68ff043bdda72c44
sriramede9 <only4programming03@gmail.com>
2019-06-15 09:05:26 -0400

    test one with working jdbc,controllers,dispather

---
+++ src/main/java/crm/jdbctest/Jdbctest.java
@@ -0,0 +1,27 @@
+package crm.jdbctest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Jdbctest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+			"jdbc:mysql://localhost:3306/giraffe","root","Hanuman");  
+			//here sonoo is database name, root is username and password  
+			Statement stmt=con.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from customer");  
+			while(rs.next())  
+			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			con.close();  
+			}catch(Exception e){ System.out.println(e);}  
+	}
+
+}
